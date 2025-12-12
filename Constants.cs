1df699c1fc98836f04e5d65a44e729ee58111f57
ZlothY29IQ <211057194+ZlothY29IQ@users.noreply.github.com>
2025-12-12 17:32:20 +0000

    its simple but works well

---
+++ Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace LegsNotArms;
+
+internal class Constants
+{
+    public const string GUID        = "zlothy.LegsNotArms";
+    public const string Name        = "LegsNotArms";
+    public const string Version     = "1.0.0";
+}
---
+++ Plugin.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace LegsNotArms;
+
+[BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
+public class Plugin : BaseUnityPlugin
+{
+    private readonly float      defaultYPosition = 0.3432f;
+    private          GameObject leftShoulder;
+    private readonly float      legsYPosition = 0.0432f;
+    private          GameObject rightShoulder;
+    private          void       Start() => GorillaTagger.OnPlayerSpawned(OnPlayerSpawned);
+
+    private void OnPlayerSpawned()
+    {
+        string shoulderPathPrefix =
+                "Player Objects/Local VRRig/Local Gorilla Player/GorillaPlayerNetworkedRigAnchor/rig/body/shoulder.";
+
+        leftShoulder  = GameObject.Find(shoulderPathPrefix + "L");
+        rightShoulder = GameObject.Find(shoulderPathPrefix + "R");
+
+        NetworkSystem.Instance.OnJoinedRoomEvent        += TurnArmsToLegsIfInModded;
+        NetworkSystem.Instance.OnReturnedToSinglePlayer += TurnArmsToLegs;
+        
+        Vector3 lPos = leftShoulder.transform.localPosition;
+        Vector3 rPos = rightShoulder.transform.localPosition;
+
+        lPos.y = legsYPosition;
+        rPos.y = legsYPosition;
+
+        leftShoulder.transform.localPosition  = lPos;
+        rightShoulder.transform.localPosition = rPos;
+    }
+
+    private void TurnArmsToLegs()
+    {
+        Vector3 lPos = leftShoulder.transform.localPosition;
+        Vector3 rPos = rightShoulder.transform.localPosition;
+
+        lPos.y = legsYPosition;
+        rPos.y = legsYPosition;
+
+        leftShoulder.transform.localPosition  = lPos;
+        rightShoulder.transform.localPosition = rPos;
+    }
+
+    private void TurnArmsToLegsIfInModded()
+    {
+        if (!NetworkSystem.Instance.GameModeString.Contains("MODDED"))
+        {
+            Vector3 lPos = leftShoulder.transform.localPosition;
+            Vector3 rPos = rightShoulder.transform.localPosition;
+
+            lPos.y = defaultYPosition;
+            rPos.y = defaultYPosition;
+
+            leftShoulder.transform.localPosition  = lPos;
+            rightShoulder.transform.localPosition = rPos;
+
+            return;
+        }
+
+        {
+            Vector3 lPos = leftShoulder.transform.localPosition;
+            Vector3 rPos = rightShoulder.transform.localPosition;
+
+            lPos.y = legsYPosition;
+            rPos.y = legsYPosition;
+
+            leftShoulder.transform.localPosition  = lPos;
+            rightShoulder.transform.localPosition = rPos;
+        }
+    }
+}
